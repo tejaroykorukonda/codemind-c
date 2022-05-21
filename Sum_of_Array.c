37bba9b97039065419487f6281ef3da5bb95f99b
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-05-21 19:45:22 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,o=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {
            o=o+arr[i];
        }
        printf("%d",o);
    }

---
+++ Sum_of_Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,o=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        
+        o=o+arr[i];
+    }
+    printf("%d",o);
+}
