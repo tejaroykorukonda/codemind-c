a2421e4849dabb6b2da5f4da4ce0a168b7a0acec
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-05-21 19:42:00 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,o=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             o=o+arr[i];         }     }     printf("%d",o); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,o=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            o=o+arr[i];
+        }
+    }
+    printf("%d",o);
+}
