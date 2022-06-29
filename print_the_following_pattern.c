830f84d44727ce0f6db91c802ade10c697c9a2d4
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-06-29 21:40:52 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {       for(j=1;j<i;j++)       {           printf(" ");       }       for(k=1;k<=n;k++)       {           if(i==1||i==n||k==1||k==n)           {               printf("*");           }           else           {               printf(" ");           }       }       printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,22 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,k;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
-        for(j=1;j<=n;j++)
-        {
-            if(i==j||(i+j)==n+1)
-            {
-                printf("x");
-            }
-            else
-            {
-                printf("0");
-            }
-        }
-        printf("
+      for(j=1;j<i;j++)
+      {
+          printf(" ");
+      }
+      for(k=1;k<=n;k++)
+      {
+          if(i==1||i==n||k==1||k==n)
+          {
+              printf("*");
+          }
+          else
+          {
+              printf(" ");
+          }
+      }
+      printf("
 ");
     }
+    
 }
