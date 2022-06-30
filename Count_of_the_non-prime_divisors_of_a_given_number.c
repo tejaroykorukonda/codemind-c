d05edf1e1bf84748c02a53c55d59395ca8c6ef66
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-06-30 21:55:18 +0530

    #include<stdio.h> int main() {     int n,i,c,p,q,j,m=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c=0;             for(j=1;j<=n;j++)             {                 if(i%j==0)                 {                     c++;                 }             }             if(c==2)             {                 continue;             }             else             {                 m++;             }         }     }     printf("%d",m); }

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c,p,q,j,m=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c=0;
+            for(j=1;j<=n;j++)
+            {
+                if(i%j==0)
+                {
+                    c++;
+                }
+            }
+            if(c==2)
+            {
+                continue;
+            }
+            else
+            {
+                m++;
+            }
+        }
+    }
+    printf("%d",m);
+}
